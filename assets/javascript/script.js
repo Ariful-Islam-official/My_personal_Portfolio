a398c15f9e6f44ad9bfef0a7a05ec6341a04b853
Ariful-Islam-official <arifislam0019@gmail.com>
2025-11-30 20:39:56 +0600

    update portfolio home section

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,9 +10,9 @@ document.getElementsByClassName("olivia_moswa")[0];
 // olivia moswa text end
 
 // designer text
-firstText = "Designer";
-secondText = "Developer";
-thridText = "Freelancer"
+firstText = "Software Engineer";
+secondText = "DevOps Engineer";
+thridText = "Network Engineer";
 intervalTime = 600;
 window.load = displayText();
 function displayText() {
